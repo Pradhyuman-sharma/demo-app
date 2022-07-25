d57842cfa936f8ebff44cdf33b6b82341b3678c9
unknown <sharmapuru0642@gmail.com>
2022-07-25 18:13:05 +0530

    demo-app test

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
+
+
+
+require('typescript-sdk/dist/integrations/express/register') ;
+process.env.KEPLOY_MODE = "test"
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -5,21 +10,21 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 app.use(cors());
 app.use(express.json());
-
-const uri = process.env.ATLAS_URI;
+function test2(){
+const uri = "mongodb://localhost:27017/test?readPreference=primary&directConnection=true&ssl=false";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+    console.log("MongoDB database connection established successfully");
 })
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
@@ -27,3 +32,8 @@ app.use('/users', usersRouter);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+}
+
+//test2()
+export default test2;
